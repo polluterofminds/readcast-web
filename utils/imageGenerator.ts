6a6486132793d1217f76f03a8d47e380638c2022
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-11 12:49:19 -0600

    error handling

--- utils/imageGenerator.ts
+++ utils/imageGenerator.ts
@@ -20,31 +20,38 @@ async function downloadImage(url: string, filepath: string) {
           .once('close', () => resolve(filepath)); 
   });
   } catch (error) {
-    console.log(error);
+    console.log("Error downloading image", error);;
+    throw error;
   }
 }
 
 export const generateBookImageOG = async (book: Book) => {
-  const tempPath = "/tmp/image.png";
-  const backgroundTemp = "/tmp/background.png"
-  const endFile = "/tmp/final.png";
-  await downloadImage(book.thumbnail, tempPath)
-  await downloadImage("https://readcast.mypinata.cloud/ipfs/QmbD72te2tUWKrfXL311Tt8CMCnc9AuSd6osX4nLB7VWZY", backgroundTemp)
-  const { data, info }: any = await sharp(tempPath)
-  .resize({
-    width: 150
-  })
-  .toBuffer({ resolveWithObject: true }) 
-  
-  await sharp(backgroundTemp)       
-      .composite([{ 
-        input: data
-      }])
-      .toFile(endFile, function(err: any) {
-        console.log("Error: ", err)
-      });
-  
-  const url = await uploadImageFromFile(endFile);
-  console.log({ url });
-  return url;
+  try {
+    const tempPath = "/tmp/image.png";
+    const backgroundTemp = "/tmp/background.png"
+    const endFile = "/tmp/final.png";
+    await downloadImage(book.thumbnail, tempPath)
+    await downloadImage("https://readcast.mypinata.cloud/ipfs/QmbD72te2tUWKrfXL311Tt8CMCnc9AuSd6osX4nLB7VWZY", backgroundTemp)
+    const { data, info }: any = await sharp(tempPath)
+    .resize({
+      width: 150
+    })
+    .toBuffer({ resolveWithObject: true }) 
+    
+    await sharp(backgroundTemp)       
+        .composite([{ 
+          input: data
+        }])
+        .toFile(endFile, function(err: any) {
+          console.log("Error: ", err)
+        });
+    
+    const url = await uploadImageFromFile(endFile);
+    console.log({ url });
+    return url; 
+  } catch (error) {
+    console.log("Image generation error")
+    console.log(error)
+    throw error;
+  }  
 }
