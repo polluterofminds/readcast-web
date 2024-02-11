c5174383a9e5ca8ef196004739860b05da79fe5a
polluterofminds <justin.edward.hunter@gmail.com>
2024-02-11 13:16:06 -0600

    using callback and more logging

--- utils/imageGenerator.ts
+++ utils/imageGenerator.ts
@@ -5,7 +5,7 @@ import { Book } from "../types";
 import fs from "fs";
 import { uploadImageFromFile } from "./ipfs";
 import axios from 'axios';
-const sharp = require('sharp')
+import sharp from 'sharp';
 
 async function downloadImage(url: string, filepath: string) {
   try {
@@ -13,12 +13,12 @@ async function downloadImage(url: string, filepath: string) {
       url,
       method: 'GET',
       responseType: 'stream'
-  });
-  return new Promise((resolve, reject) => {
+    });
+    return new Promise((resolve, reject) => {
       response.data.pipe(fs.createWriteStream(filepath))
-          .on('error', reject)
-          .once('close', () => resolve(filepath)); 
-  });
+        .on('error', reject)
+        .once('close', () => resolve(filepath));
+    });
   } catch (error) {
     console.log("Error downloading image", error);;
     throw error;
@@ -27,32 +27,35 @@ async function downloadImage(url: string, filepath: string) {
 
 export const generateBookImageOG = async (book: Book) => {
   try {
-    const tempPath = "/tmp/image.jpg";
-    const backgroundTemp = "/tmp/background.jpg"
-    const endFile = "/tmp/final.jpg";
+    const tempPath = "./tmp/image.jpg";
+    const backgroundTemp = "./tmp/background.jpg"
+    const endFile = "./tmp/final.jpg";
     await downloadImage(book.thumbnail, tempPath)
     await downloadImage("https://readcast.mypinata.cloud/ipfs/QmbD72te2tUWKrfXL311Tt8CMCnc9AuSd6osX4nLB7VWZY", backgroundTemp)
+
+    console.log(await fs.readdirSync("./tmp/"))
+
     const { data, info }: any = await sharp(tempPath)
-    .resize({
-      width: 150
-    })
-    .toBuffer({ resolveWithObject: true }) 
-    
-    await sharp(backgroundTemp)       
-        .composite([{ 
-          input: data
-        }])
-        .toFile(endFile, function(err: any) {
-          console.log("Error: ", err)
-        });
-    const metadata = await sharp(endFile).metadata();
-    console.log({metadata});
-    const url = await uploadImageFromFile(endFile);
-    console.log({ url });
-    return url; 
+      .resize({
+        width: 150
+      })
+      .toBuffer({ resolveWithObject: true })
+
+    sharp(backgroundTemp)
+      .composite([{
+        input: data
+      }])
+      .toFile(endFile, async function (err: any) {
+        console.log("Error: ", err)
+        const metadata = await sharp(endFile).metadata();
+        console.log({ metadata });
+        const url = await uploadImageFromFile(endFile);
+        console.log({ url });
+        return url;
+      })
   } catch (error) {
     console.log("Image generation error")
     console.log(error)
     throw error;
-  }  
+  }
 }
